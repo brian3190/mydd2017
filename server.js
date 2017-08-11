e6d7099e1c3b08b4343691e813497d2d2142a232
brian3190 <brian3190@gmail.com>
2017-08-11 18:29:48 +0800

    small fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname + /'index.html'));
+  res.sendFile(path.join(__dirname + 'index.html'));
 });
 
 app.listen(3000, () => {
